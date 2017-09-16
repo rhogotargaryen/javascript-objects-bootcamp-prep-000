084587e145982e30ae9edbbcf879b1f8d83498c0
Derek Haller <dhaller.personal@gmail.com>
2017-09-16 00:12:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,7 @@ function updatePlaylist (object, artist, songTitle) {
     object[artist] = songTitle;
     return object;
   }
+
+function removeFromPlaylist(object, artist) {
+  delete object[artist];
+}
